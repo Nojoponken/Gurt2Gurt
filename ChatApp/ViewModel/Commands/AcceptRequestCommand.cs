f40675388b18dc424b7c4d164ecc39b6681be64e
Giedre Aleksandrauskaite <gieal640@student.liu.se>
2023-12-13 18:23:07 +0100

    Accept and Deny request command class

---
+++ ChatApp/ViewModel/Commands/AcceptRequestCommand.cs
@@ -0,0 +1,32 @@
+﻿using ChatApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChatApp.ViewModel.Commands
+{
+    internal class AcceptRequestCommand : ICommand
+    {
+        private readonly ChatScreenViewModel parent;
+
+        public AcceptRequestCommand(ChatScreenViewModel parent)
+        {
+            this.parent = parent;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            parent.AcceptIncoming();
+        }
+    }
+}
