26b90b16269bfeb783df8534dd4e24ed98d6563a
Giedre Aleksandrauskaite <gieal640@student.liu.se>
2023-12-16 17:49:53 +0100

    här är buzz classen också

---
+++ ChatApp/ViewModel/Commands/BuzzCommand.cs
@@ -0,0 +1,32 @@
+﻿using ChatApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChatApp.ViewModel.Commands
+{
+    internal class BuzzCommand : ICommand
+    {
+        private readonly ChatScreenViewModel parent;
+
+        public BuzzCommand(ChatScreenViewModel parent)
+        {
+            this.parent = parent;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            parent.SendBuzz();
+        }
+    }
+}
